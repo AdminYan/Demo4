d8e6a935554fbcdf50a918683fa0cd8f2464f9dc
AdminYan <62363887+AdminYan@users.noreply.github.com>
2020-03-19 16:14:49 +0800

    Update MainActivity.java

--- app/src/main/java/com/bw/demo4/MainActivity.java
+++ app/src/main/java/com/bw/demo4/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+         //吐司提示
         Toast.makeText(this,"第一次",Toast.LENGTH_SHORT).show();
 
     }
